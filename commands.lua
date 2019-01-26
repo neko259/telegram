f67191ca37a467c96dec946c81374fef6829930d
neko259 <neko259@gmail.com>
2019-01-26 21:04:48 +0200

    If no players are on server, show a separate message instead of reporting an empty list

--- commands.lua
+++ commands.lua
@@ -8,11 +8,17 @@ end)
 
 telegram.register_command("players", function(msg)
     local players = ""
-    for _,player in ipairs(minetest.get_connected_players()) do
-        local name = player:get_player_name()
-        players = players .. player:get_player_name() .. ", "
+
+    local player_list = minetest.get_connected_players()
+    if #player_list > 0 then
+        for _,player in ipairs(minetest.get_connected_players()) do
+            local name = player:get_player_name()
+            players = players .. player:get_player_name() .. ", "
+        end
+        telegram.send_message(msg.chat.id, "Active players: " .. players)
+    else
+        telegram.send_message(msg.chat.id, "No active players detected." .. players)
     end
-    telegram.send_message(msg.chat.id, "Active players: " .. players)
 end)
 
 telegram.register_command("status", function(msg)
